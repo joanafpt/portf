6bec47a8ae5345d71b3d8efe6ea852b0da4e4cb1
Joana Teixeira <joanajteixeira@outlook.com>
2020-06-15 17:44:22 +0100

    Updates

---
+++ precache-manifest.cf53ccfdcdeb7e77e3705a8b873d85ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76284c563dbbcae6c3b5b063dd32cae4",
+    "url": "/portf/index.html"
+  },
+  {
+    "revision": "33c831578f08ffc612a5",
+    "url": "/portf/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0a6cc00c6c4d8a46e69d",
+    "url": "/portf/static/css/main.2a189968.chunk.css"
+  },
+  {
+    "revision": "33c831578f08ffc612a5",
+    "url": "/portf/static/js/2.e5870c56.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portf/static/js/2.e5870c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6cc00c6c4d8a46e69d",
+    "url": "/portf/static/js/main.601a36e1.chunk.js"
+  },
+  {
+    "revision": "40227bebd23f426c10ac",
+    "url": "/portf/static/js/runtime-main.1f4ab5a6.js"
+  }
+]);
